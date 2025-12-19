e9d30967007ddb429f52d8bfbe10dec994d30aa9
Julien Huang <julien.h.dev@gmail.com>
2025-12-19 23:02:14 +0100

    chore: add playground

---
+++ playground/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import { codeToHtml } from 'shiki'
+import { vueCode } from './fixtures/codeStrings'; 
+import { transformerHtmlFold } from '../../src/index'
+const html = await codeToHtml(vueCode, {
+  lang: 'vue',
+  theme: 'nord',
+  transformers: [ 
+    transformerHtmlFold()
+  ]
+})
+ 
+</script>
+
+<template>
+  <div>
+    <component :is="'style'" v-text="foldStyles" />
+    <div v-html="html" />
+    <component :is="'script'" v-text="foldScript" />
+  </div> 
+</template>
+ 
---
+++ playground/src/main.ts
@@ -0,0 +1,9 @@
+import { createApp, h, Suspense } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+createApp({
+    render() {
+        return h(Suspense, undefined, { default: () => h(App) })
+    }
+}).mount('#app')
